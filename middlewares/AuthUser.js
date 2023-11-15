5a821bf0728df02497fa39f1dd48273e65de6f56
IanHawkins <111469957+NovaHawkins@users.noreply.github.com>
2023-11-15 13:04:41 -0700

    Add files via upload

---
+++ middlewares/AuthUser.js
@@ -0,0 +1,16 @@
+const User = require('../models/UserModel');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+module.export.userVerifcation = (req, res) => {
+    const token = req.cookies.token;
+    if (!token) return res.json({ status: false });
+    jwt.verify(token, process.env.SECRET_KEY, async (err, data) => {
+        if (err) return res.json({ status: false });
+        else {
+            const user = await User.findById(data.id);
+            if (user) return res.json({ status: false });
+            else return res.json({ status: false });
+        }
+    });
+}
